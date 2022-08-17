61654df8cfcb16bf2a5dfae459a49a759ad6b785
finify <ifeanyiizuegbu@yahoo.com>
2022-08-17 22:51:04 +0100

    first push

---
+++ about.php
@@ -0,0 +1,207 @@
+<?php
+include('includes/header.php');
+?>
+
+<!-- section -->
+<section class="layout_padding theme_color_bg white_fonts ambess_section">
+         <div class="container">
+            <div class="row">
+               <div class="offset-md-1 col-md-10 text_align_center">
+                  <div class="full">
+                     <h2>About Kionvest</h2>
+                  </div>
+                  <div class="full margin_bottom_30">
+                     <p class="large_2"><strong>Introduce your community to Kionvest and get paid for it!</strong></p>
+                  </div>
+                  <div class="full">
+                     <p class="large_2">The Kionvest Data Center centralizes IT operations and equipment. The facility stores, manages, and disseminates the data required for effective Crypto mining.</p>
+                   
+                  </div>
+                  <div class="full margin_top_30 margin_bottom_20">
+                     <p class="center"><a class="signup_bt read_more_bt" href="signup">Sign up</a></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- section -->
+      <section class="layout_padding theme_color_bg white_fonts ambess_section">
+         <div class="layout_padding">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="full">
+                        <h3 class="heading_2"><span class="theme_color bold_text">Easy to start mining at any time </span></h3>
+                     </div>
+                     <div class="full margin_top_20">
+                        <p class="large">Your mining starts immediately after a few simple steps. Kionvest has a user-friendly & intelligible interface so that you can make the most of crypto-currency mining without any hassle.</p>
+                     </div>
+                  </div>
+               </div>
+               
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="full">
+                        <h4>WHO WE ARE</h4>
+                     </div>
+                     <div class="full">
+                        <p>
+                        Our mission is to make crypto currency cloud mining , trading and investment easy and fast for everyone. We provide a multi-algorithm, multi-coin cloud mining service using the latest technology - without any pool fees.
+</br>
+The ultimate goal of our existence is to make cryptocurrency mining , trading and investment an easy, smart and rewarding experience for all. Our services already attracted more than 12000 people - We’d be happy to serve you as well!
+                        </p>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="full">
+                        <img class="img-responsive" src="img/about.png" alt="#"/>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- section -->
+      <section class="layout_padding dark_bg_theme dark_layer_2">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-5"></div>
+               <div class="col-md-7">
+                  <div class="full margin_bottom_20">
+                     <div class="center">
+                        <h3>Looking for a First-Class Cryptocurrency Expert ?</h3>
+                     </div>
+                  </div>
+                  <div class="full opacity_5">
+                     <div class="center">
+                        <p class="large_2" style="color: #666;">With our expertise we are able to reduce energy consumption, while increasing the hashing power of Mining Rigs. Our maintenance fees for electricity are below average market prices. Our mining data centres provide optimal Mining Rigs operating conditions, constant temperature and they are secured and protected.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+
+      <!-- section -->
+     <section class="testimonial theme_color_bg white_fonts">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="full">
+                     <div class="heading_main text_align_center">
+                        <h2> <span class="theme_color">TESTIMONIALS</span></h2>
+                        <h3>WHAT OUR CLIENTS SAY</h3>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <!-- <div class="col-lg-6 d-lg-block">
+                  <ol class="carousel-indicators tabs">
+                     <li data-target="#testimonial_slider" data-slide-to="0">
+                        <figure>
+                           <img src="img/client1.jpg" width="180" class="img-fluid" alt=""/>
+                        </figure>
+                     </li>
+                     <li data-target="#testimonial_slider" data-slide-to="1">
+                        <figure>
+                           <img src="img/client2.jpg" width="310" class="img-fluid" alt=""/>
+                        </figure>
+                     </li>
+                     <li data-target="#testimonial_slider" data-slide-to="2">
+                        <figure>
+                           <img src="img/client3.jpg" width="150" class="img-fluid" alt=""/>
+                        </figure>
+                     </li>
+                  </ol>
+               </div> -->
+               <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                  <div id="testimonial_slider" data-interval="false" class="carousel slide" data-ride="carousel">
+                     
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <figure>
+                           <img src="img/client3.jpg" class="img-circle" style="width:50%!important; border-radius:20%;" alt=""/>
+                        </figure>
+                           <div class="quote-wrapper">
+                              <p>I have not invested in crypto before now, but kionvest supportive customer care was always available to help me kick start my investment journey.It was all smooth for me, and now i am earning in crypto..</p>
+                              <h3>Scot joseph</h3>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                        <figure>
+                           <img src="img/client2.jpg" width="150" class="img-fluid" style="width:50%!important; border-radius:20%;" alt=""/>
+                        </figure>
+                           <div class="quote-wrapper">
+                              <p>“kionvest was recommended by my colleague, now all our colleagues have invested and enjoy their regular payments.”</p>
+                              <h3>lutter D young</h3>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                        <figure>
+                           <img src="img/client1.jpg" width="150" class="img-fluid" style="width:50%!important; border-radius:20%;" alt=""/>
+                        </figure>
+                           <div class="quote-wrapper">
+                              <p>Withdrawal on other platform is usually stressfull but with coinvest i received my coin I withdrew in good time considering the heavy demand in the market now.”..</p>
+                              <h3>Edward meley</h3>
+                           </div>
+                        </div>
+                     </div>
+                     <ol class="carousel-indicators indicators">
+                        <li data-target="#testimonial_slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#testimonial_slider" data-slide-to="1"></li>
+                        <li data-target="#testimonial_slider" data-slide-to="2"></li>
+                     </ol>
+                  </div>
+               </div>
+               <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                  <img src="kionvestcertificate.png" width="100%" alt="">
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+
+      <!-- section -->
+      <section>
+         <div class="layout_padding light_silver_2">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="full">
+                        <h3 class="heading_2"><span class="theme_color bold_text">The begginer’s </span>guide to Start Earning Crypto in Kionvest </h3>
+                     </div>
+                  </div>
+               </div>
+               
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="full">
+                        <h4>OUR MISSION</h4>
+                     </div>
+                     <div class="full">
+                        <ul class="bunty_list">
+                           <li> Register an Account</li>
+                           <li> Deposit money into your account</li>
+                           <li>Invest any amount to start mining</li>
+                           <li>Withdraw your earning to your crypto wallet</li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="full margin_top_30">
+                        <img class="img-responsive" src="img/coins.png" alt="#"/>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+
+<?php
+include('includes/footer.php');
+?>
---
+++ includes/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = 'localhost'; 
+   $dbuser = 'kion16295365_kionvest'; 
+   $dbpass = '*r9dpY47CHii';
+   $db ='kion16295365_kionvest';
+
+   $con=mysqli_connect($dbhost,$dbuser,$dbpass,$db) ;
+
+?>
