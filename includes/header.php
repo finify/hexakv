e35e978c852a2008bb5da3a0c0c944a495b5397c
finify <ifeanyiizuegbu@yahoo.com>
2022-08-17 23:21:22 +0100

    smartsup

--- includes/header.php
+++ includes/header.php
@@ -32,6 +32,17 @@ require('includes/dbconnect.php');//DBCONNECTION
    <link rel='canonical' href='index.php' />
 
 
+   <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b291769e2f46936544f45920f2c6ffb891267f61';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
 </head>
    <body class="home">
       <!-- header -->
